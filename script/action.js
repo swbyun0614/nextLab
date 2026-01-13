d8d1febe77a1971e74c221baafd578f3aa01da19
swbyun0614 <swbyun0614@gmail.com>
2026-01-13 10:48:00 +0900

    header all fin

--- script/action.js
+++ script/action.js
@@ -7,13 +7,4 @@ $(function() {
     $('header').mouseleave(function(){
         $('.lnb, .lnb_bg').stop().slideUp(200);
     });
-
-    /* (선택사항) 이미지 교체를 스크립트로 더 확실하게 하고 싶을 경우 */
-    /*
-    $('.blog a').hover(function(){
-        $(this).find('img').attr('src', 'images/btn_go_hover.png');
-    }, function(){
-        $(this).find('img').attr('src', 'images/btn_go.png');
-    });
-    */
 });
