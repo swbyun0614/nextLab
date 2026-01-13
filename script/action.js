ad0adda9850e8cc1c3cd0ea48358cc3d9274571a
swbyun0614 <swbyun0614@gmail.com>
2026-01-13 11:01:43 +0900

    header hover fin

--- script/action.js
+++ script/action.js
@@ -1,6 +1,6 @@
 $(function() {
     // 서브메뉴 슬라이드 제어
-    $('.gnb, .lnb_bg').mouseenter(function(){
+    $('header').mouseenter(function(){
         $('.lnb, .lnb_bg').stop().slideDown(200); 
     });
 
