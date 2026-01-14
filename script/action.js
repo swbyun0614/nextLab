a7af93f00a6cc1f49f0b9c1fcd8cc3fabf0e2202
swbyun0614 <swbyun0614@gmail.com>
2026-01-14 15:45:01 +0900

    btnTop

--- script/action.js
+++ script/action.js
@@ -1,5 +1,5 @@
+/* 서브메뉴 슬라이드 제어 */
 $(function() {
-    // 서브메뉴 슬라이드 제어
     $('header').mouseenter(function(){
         $('.lnb, .lnb_bg').stop().slideDown(200); 
     });
@@ -7,4 +7,11 @@ $(function() {
     $('header').mouseleave(function(){
         $('.lnb, .lnb_bg').stop().slideUp(200);
     });
+});
+
+
+/* TOP 버튼 */
+$('.btnTop').click(function(){
+    $('html, body').animate({scrollTop:0}, 500)
+    // 보통 html과 body를 같이 씀
 });
